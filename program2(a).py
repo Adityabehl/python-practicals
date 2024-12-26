31c393df97a9f2156ef981c57a23ffdced762fa5
Adityabehl <adityabehl0611@gmail.com>
2024-12-26 13:06:53 +0530

    Update program2(a).py

--- program2(a).py
+++ program2(a).py
@@ -2,12 +2,10 @@
 n=int(input("enter number to be checked:"))
 def prime(n):
   flag=0
-  for i in range(2,int(n**0.5)+1):
+  for i in range(2,n):
     if n%i==0:
       flag=1
       break
-    else:
-      flag=0
   if flag==0:
     print("prime")
   else:
